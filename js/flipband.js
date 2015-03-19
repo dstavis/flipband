89d229f18bd680bcca7bb27bb9dc50fade597d33
David Stavis <davidstavis@Davids-MacBook-Pro.local>
2015-03-18 17:33:46 -0700

    FEATURE: Asks for proof before flipping! Plus: Prettier font & Bootstrap

--- js/flipband.js
+++ js/flipband.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	setup()
-	$('body').click(flip)
+	$('body').click(clickHandler)
 })
 
 var setup = function(){
@@ -13,13 +13,52 @@ var setup = function(){
 	}
 }
 
-var flip = function(){
+var clickHandler = function(){
 	if (localStorage.getCacheItem("flipped") == "flipped"){
 		console.log("I'm already flipped yo. Wait until tomorrow!")
 	} else {
-		$('body').removeClass('not-done')
-		$('.message').html("")
-		$('body').addClass('done')
-		localStorage.setCacheItem("flipped", "flipped", {days: 1})
+		askForProof()
 	}
+}
+
+var cancelFlip = function(){
+	localStorage["flipped"] = null;
+}
+
+var askForProof = function(){
+	$("#proof-holder").show()
+	$("#submit-proof").click(proofHandler)
+}
+
+var proofHandler = function(){
+	var proof = $("#proof").val()
+	if (saveProof(proof)){
+		$('.message').html("")
+		$("#proof-holder").hide()
+		flip()
+	}
+}
+
+var saveProof = function(proof){
+	// Proof must be a string with some text
+	if ( typeof proof == 'string' && (proof != null && proof != undefined && proof != "")){
+		var today = moment().format("MM/DD/YY")
+		var saveData = {}
+		saveData[today] = { data: proof, time: moment().toDate() }
+		localStorage["flipband-proof"] = JSON.stringify(saveData)
+		return true;
+	} else {
+		console.log("Proof must be a string with something in it")
+		return false;
+	}
+}
+
+var getProof = function(){
+	return JSON.parse(localStorage["flipband-proof"])
+}
+
+var flip = function(){
+	$('body').removeClass('not-done')
+	$('body').addClass('done')
+	localStorage.setCacheItem("flipped", "flipped", {days: 1})
 }
